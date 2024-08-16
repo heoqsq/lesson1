487d9e735d060051530865c2bda05cd9554f51f5
Sarkis <sarkismihail@yandex.ru>
2024-08-16 22:22:57 +0300

    add lesson1:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = 60;
+int b = 70;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
